3c6b2b5d763c4afd4c565f1192e4cb7fb67d3059
Nikolas Schnellbächer <khx0@posteo.net>
2020-10-17 23:06:11 +0200

    added pcost exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+
+filename = 'Data/portfolio.csv'
+
+total_cost = 0.0
+
+with open(filename, 'rt') as f:
+    header = next(f)
+    for line in f:
+        row = line.split(',')
+        n_stocks, price = int(row[1]), float(row[2])
+        total_cost += n_stocks * price
+
+print(f'Total cost {total_cost:5.2f}')    
