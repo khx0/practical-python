f37ef0b4d946c265e375cc898e6efc56a0f1521b
Nikolas Schnellbächer <khx0@posteo.net>
2020-10-22 21:41:25 +0200

    added report_exercise_2.5.py

--- Work/report_exercise_2.5.py
+++ Work/report_exercise_2.5.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.5
 
 import csv
 
@@ -12,12 +12,11 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {
+                'name':row[0], \
+                'shares': int(row[1]), \
+                'price': float(row[2])
+            }
             portfolio.append(holding)
 
     return portfolio
-
-portfolio = read_portfolio('Data/portfolio.csv')
-
-for p in portfolio:
-    print(p)
