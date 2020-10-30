61d7d93f9e8d02c0a5e552d0bb041d111f3d97f0
Nikolas Schnellbächer <khx0@posteo.net>
2020-10-30 19:34:02 +0100

    updated report_exercise_2.16.py

--- Work/report_exercise_2.16.py
+++ Work/report_exercise_2.16.py
@@ -4,22 +4,16 @@
 
 import csv
 
-def read_prices(filename):
-    prices = {}
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        for row in rows:
-            try:
-                name, price = row[0], float(row[1])
-                prices[name] = price
-            except: pass
-    return prices
-
 def read_portfolio(filename):
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
     portfolio = []
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         header = next(rows)
+
         for row in rows:
             holding = dict(zip(header, row))
             try: 
@@ -27,15 +21,36 @@ def read_portfolio(filename):
                 holding['shares'] = int(holding['shares'])
             except: pass
             portfolio.append(holding)
+
     return portfolio
 
+def read_prices(filename):
+    '''
+    Read a CSV file of price data into a dict mapping names to prices.
+    '''
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                name, price = row[0], float(row[1])
+                prices[name] = price
+            except IndexError:
+                pass
+
+    return prices
+
 def make_report(portfolio, prices):
-    report = []
+    '''
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    '''
+    rows = []
     for holding in portfolio:
-        new_price = prices[holding['name']]
-        change = new_price - holding['price']
-        holding = (holding['name'], holding['shares'], new_price, change)
-        report.append(holding)
+        current_price = prices[holding['name']]
+        change = current_price - holding['price']
+        summary = (holding['name'], holding['shares'], current_price, change)
+        rows.append(summary)
     return report
 
 portfolio = read_portfolio('Data/portfolio.csv')
